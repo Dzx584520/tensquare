bd6bc6f8bce7e412f9d503c79774c75617b779ea
584321579 <584321579@qq.com>
2021-01-08 15:00:33 +0800

    增删改查Controller

---
+++ tensquare_base/src/main/java/com/tensquare/controller/LableController.java
@@ -0,0 +1,50 @@
+package com.tensquare.controller;
+
+import com.tensquare.pojo.Label;
+import com.tensquare.service.LabelService;
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin //跨域问题解决
+@RequestMapping(value = "/label")
+public class LableController {
+
+    @Autowired
+    private LabelService labelService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Result findAll(){
+        List<Label> all = labelService.findAll();
+        return new Result(true, StatusCode.OK,"message",all);
+    }
+
+    @RequestMapping(value = "/{labelId}",method = RequestMethod.GET)
+    public Result findById(@PathVariable("labelId") String labelId){
+        Label label = labelService.findById(labelId);
+        return new Result(true, StatusCode.OK,"message",label);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Result save(@RequestBody Label label){
+        labelService.save(label);
+        return new Result(true, StatusCode.OK,"添加成功");
+    }
+
+    @RequestMapping(value = "/{labelId}",method = RequestMethod.DELETE)
+    public Result delete(@PathVariable("labelId") String labelId){
+        labelService.delete(labelId);
+        return new Result(true, StatusCode.OK,"删除成功");
+    }
+
+    @RequestMapping(value = "/{labelId}",method = RequestMethod.PUT)
+    public Result update(@PathVariable String labelId, @RequestBody Label label){
+        label.setId(labelId);
+        labelService.update(label);
+        return new Result(true, StatusCode.OK,"更新成功");
+    }
+}
